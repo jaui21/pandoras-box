dd84bc45734c90eeb53527424f4afac281d91428
jaui21 <joeylana@ijaui.com>
2016-05-22 05:08:21 +0800

    Create config.php

---
+++ PHP/braintree-integration/lib/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array(
+
+	/*
+	* ADMIN ACCESS CREDENTIALS
+	*/
+	'admin_username' => 'admin', // username used to login to the admin area
+	'admin_password' => 'admin', // password used to login to the admin area
+
+	/*
+	* DATABASE ACCESS CREDENTIALS
+	*/
+	'db_host' => '[Your database host]', // database host, usually localhost
+	'db_username' => '[Your database username]', // database username
+	'db_password' => '[Your database password]', // datebase password
+	'db_name' => '[Your database name]', // database name
+
+);
